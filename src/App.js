af0c6608ae2390b2f0f54f494092c6129acf14b4
Lawrence <locaulker@gmail.com>
2020-05-05 01:17:28 -0400

    added navbar-component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import './App.css'
-import Home from './pages/Home'
-import Rooms from './pages/Rooms'
-import SingleRoom from './pages/SingleRoom'
-import Error from './pages/Error'
+import React from "react"
+import "./App.css"
+import Home from "./pages/Home"
+import Rooms from "./pages/Rooms"
+import SingleRoom from "./pages/SingleRoom"
+import Error from "./pages/Error"
 
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 function App() {
 	return (
 		<>
+			<Navbar />
 			<Switch>
 				<Route exact path="/" component={Home} />
 				<Route path="/rooms/" component={Rooms} />
